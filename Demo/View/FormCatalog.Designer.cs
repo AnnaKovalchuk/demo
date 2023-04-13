c08df89bdd747fbef1d7d5c3ae14d95613a60018
Anya <anaykovalchuk00@mail.ru>
2023-04-13 18:36:24 +0300

    end

---
+++ Demo/View/FormCatalog.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Demo
+{
+    partial class FormCatalog
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCatalog));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxCost = new System.Windows.Forms.ComboBox();
+            this.comboBoxDiscount = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Pic = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Disc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price_without_discount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Discount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price_with_discount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonExit, 0, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 68F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 320F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 93F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 701F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(794, 62);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(96, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(695, 62);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Каталог";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Demo.Properties.Resources.logo;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(87, 56);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 5;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.46851F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 19.39547F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.71285F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.01007F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 38.79093F));
+            this.tableLayoutPanel3.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.comboBoxCost, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.comboBoxDiscount, 3, 0);
+            this.tableLayoutPanel3.Controls.Add(this.label3, 2, 0);
+            this.tableLayoutPanel3.Controls.Add(this.labelCount, 4, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 71);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(794, 26);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 26);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Стоимость";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboBoxCost
+            // 
+            this.comboBoxCost.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBoxCost.FormattingEnabled = true;
+            this.comboBoxCost.Items.AddRange(new object[] {
+            "По убыванию",
+            "По возрастанию"});
+            this.comboBoxCost.Location = new System.Drawing.Point(101, 3);
+            this.comboBoxCost.Name = "comboBoxCost";
+            this.comboBoxCost.Size = new System.Drawing.Size(147, 24);
+            this.comboBoxCost.TabIndex = 0;
+            this.comboBoxCost.SelectedIndexChanged += new System.EventHandler(this.comboBoxCost_SelectedIndexChanged);
+            // 
+            // comboBoxDiscount
+            // 
+            this.comboBoxDiscount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBoxDiscount.FormattingEnabled = true;
+            this.comboBoxDiscount.Items.AddRange(new object[] {
+            "Все диапазоны",
+            "0% - 9%",
+            "10% - 15%",
+            "15% и более"});
+            this.comboBoxDiscount.Location = new System.Drawing.Point(346, 3);
+            this.comboBoxDiscount.Name = "comboBoxDiscount";
+            this.comboBoxDiscount.Size = new System.Drawing.Size(136, 24);
+            this.comboBoxDiscount.TabIndex = 0;
+            this.comboBoxDiscount.SelectedIndexChanged += new System.EventHandler(this.comboBoxDiscount_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Location = new System.Drawing.Point(254, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 26);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Скидка";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelCount.Location = new System.Drawing.Point(488, 0);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(303, 26);
+            this.labelCount.TabIndex = 0;
+            this.labelCount.Text = "labelCount";
+            this.labelCount.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Pic,
+            this.Disc,
+            this.Price_without_discount,
+            this.Discount,
+            this.Price_with_discount});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 104);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(792, 312);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Pic
+            // 
+            this.Pic.FillWeight = 150F;
+            this.Pic.HeaderText = "Изображение";
+            this.Pic.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+            this.Pic.MinimumWidth = 6;
+            this.Pic.Name = "Pic";
+            this.Pic.ReadOnly = true;
+            this.Pic.Width = 150;
+            // 
+            // Disc
+            // 
+            this.Disc.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.Disc.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Disc.HeaderText = "Описание";
+            this.Disc.MinimumWidth = 6;
+            this.Disc.Name = "Disc";
+            this.Disc.ReadOnly = true;
+            // 
+            // Price_without_discount
+            // 
+            this.Price_without_discount.HeaderText = "Цена без скидки";
+            this.Price_without_discount.MinimumWidth = 6;
+            this.Price_without_discount.Name = "Price_without_discount";
+            this.Price_without_discount.ReadOnly = true;
+            this.Price_without_discount.Width = 80;
+            // 
+            // Discount
+            // 
+            this.Discount.HeaderText = "Скидка";
+            this.Discount.MinimumWidth = 6;
+            this.Discount.Name = "Discount";
+            this.Discount.ReadOnly = true;
+            this.Discount.Width = 80;
+            // 
+            // Price_with_discount
+            // 
+            this.Price_with_discount.HeaderText = "Цена со скидкой";
+            this.Price_with_discount.MinimumWidth = 6;
+            this.Price_with_discount.Name = "Price_with_discount";
+            this.Price_with_discount.ReadOnly = true;
+            this.Price_with_discount.Width = 80;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonExit.Location = new System.Drawing.Point(3, 423);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(794, 24);
+            this.buttonExit.TabIndex = 3;
+            this.buttonExit.Text = "Выход";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // FormCatalog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormCatalog";
+            this.Text = "Каталог";
+            this.Load += new System.EventHandler(this.FormCatalog_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxCost;
+        private System.Windows.Forms.ComboBox comboBoxDiscount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewImageColumn Pic;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Disc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price_without_discount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Discount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price_with_discount;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ Demo/View/FormCatalog.cs
@@ -0,0 +1,123 @@
+﻿using Demo.Classes;
+using Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace Demo
+{
+    public partial class FormCatalog : Form
+    {
+        public FormCatalog()
+        {
+            InitializeComponent();
+        }
+
+        public static string picName;
+        public static string path = Application.StartupPath + "/Pict/";
+        public static Bitmap bitmap;
+        /// <summary>
+        /// Метод для заполнения каталога
+        /// </summary>
+        private void LoadDataDrid()
+        {
+            dataGridView1.Rows.Clear();
+            using (DB01Entities DB = new DB01Entities())
+            {
+                var products = DB.Product.ToList();
+
+                //Сортировка по цене
+                if (comboBoxCost.SelectedIndex == 0)
+                    products = products.OrderByDescending(x => x.ProductCost).ToList();
+                else if (comboBoxCost.SelectedIndex == 1)
+                    products = products.OrderBy(x => x.ProductCost).ToList();
+
+                //Сортировка по скидке
+                if (comboBoxDiscount.SelectedIndex == 1)
+                    products = products.Where(x => (x.ProductDiscount < 10)).ToList();
+                else if (comboBoxDiscount.SelectedIndex == 2)
+                    products = products.Where(x => (x.ProductDiscount > 10) && x.ProductDiscount < 15).ToList();
+                else if (comboBoxDiscount.SelectedIndex == 3)
+                    products = products.Where(x => (x.ProductDiscount >= 15)).ToList();
+
+                int i = 0;
+                foreach (var item in products)
+                {
+                    dataGridView1.Rows.Add();
+
+                    picName = item.ProductPhoto;
+
+                    if (String.IsNullOrEmpty(picName))
+                        dataGridView1.Rows[i].Cells[0].Value = Properties.Resources.nophoto;
+                    else
+                    {
+                        bitmap = new Bitmap(path + picName);
+                        dataGridView1.Rows[i].Cells[0].Value = bitmap;
+                    }
+
+                    if(item.ProductDiscount != null)
+                    {
+                        dataGridView1.Rows[i].Cells[1].Value =
+                        $"Название: {item.ProductName}" +
+                        $"\nОписание: {item.ProductDescription}" +
+                        $"\nПроизводитель: {item.Manufacturer.ManufacturerName}";
+                        dataGridView1.Rows[i].Cells[2].Value = $"\n{item.ProductCost}";
+                        dataGridView1.Rows[i].Cells[3].Value = $"\n{item.ProductDiscount}" + " %";
+                        dataGridView1.Rows[i].Cells[4].Value = $"\n{item.ProductCost * (100-item.ProductDiscount) / 100}" ;
+                        dataGridView1.Rows[i].Cells[2].Style = new DataGridViewCellStyle()
+                        {
+                            Font = new Font(this.Font, FontStyle.Strikeout)
+                        };
+                    }
+                    else
+                    {
+                        dataGridView1.Rows[i].Cells[1].Value =
+                        $"Название: {item.ProductName}" +
+                        $"\nОписание: {item.ProductDescription}" +
+                        $"\nПроизводитель: {item.Manufacturer.ManufacturerName}";
+                        dataGridView1.Rows[i].Cells[2].Value = $"\n{item.ProductCost}";
+                        dataGridView1.Rows[i].Cells[3].Value = $"\n-";
+                        dataGridView1.Rows[i].Cells[4].Value = $"\n-";
+                    }
+
+                    if (item.ProductDiscount >= 15)
+                        dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.Red;
+                    i++;
+                }
+
+                labelCount.Text = "Количество записей: " + i.ToString();
+            }
+        }
+
+        private void FormCatalog_Load(object sender, EventArgs e)
+        {
+            comboBoxCost.SelectedIndex = 0;
+            comboBoxDiscount.SelectedIndex = 0;
+            LoadDataDrid();
+        }
+
+        private void comboBoxCost_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadDataDrid();
+        }
+
+        private void comboBoxDiscount_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadDataDrid();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Вы действительно хотите выйти?", "Подтверждение выхода", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+                this.Close();
+        }
+    }
+}
